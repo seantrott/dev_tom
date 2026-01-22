e872070a647ad6a77fc1d978df6c9d89beeed3b4
pdrivier <33138648+pdrivier@users.noreply.github.com>
2026-01-22 08:08:58 -0800

    wip: script to get attn scores by roi

---
+++ src/analysis/aggreggate-attn-by-roi.py
@@ -0,0 +1,62 @@
+# Here, you'll grab the dataframe of unique false belief passages annotated by region of interest, as 
+# well as each LM's checkpoint-layer-head's attention scores for each token in the passage (this is 
+# attention from the final token to every other token in the passage). 
+
+# You'll then output a dataframe containing, among others, these key columns:
+# MODEL_SHORTHAND | STEP | LAYER | HEAD | PASSAGE | TOKENIZED_PASSAGE | AVG_ATTN_SETUP | AVG_ATTN_EXIT | ...
+# AVG_ATTN_MANIPULATION | AVG_ATTN_RETURN | AVG_ATTN_QUERY
+
+
+import os 
+
+import numpy as np
+import pandas as pd
+from pprint import pprint
+
+
+# Define a function that cleans up the attention scores (which for some reason are interpreted as strings rather than numpy arrays or lists)
+def parse_attention_string(s):
+    """
+    Convert a stringified numpy array to a 1D numpy array.
+    """
+    if isinstance(s, np.ndarray):
+        return s
+    if isinstance(s, list):
+        return np.asarray(s)
+    return np.fromstring(s.strip("[]"), sep=" ")
+
+# Define a function to average attention scores over regions of interest in false belief passages
+def mean_attn_roi(attn, span): 
+	""" attn: list or 1D array of attention scores
+		span: tuple (start, end)"""
+	if span is None or len(span) != 2:
+		return np.nan 
+	start, end = span
+	# spans are inclusive, so use end + 1
+	return float(np.mean(attn[start:end + 1]))
+
+
+# Load the annotated passages
+path_annot = "../../data/raw/fb_annotated_passages.csv"
+df_annot = pd.read_csv(path_annot)
+
+# Load the attention scores data and bind them all into a single dataframe
+path_scores = "../../data/processed/fb_attention_scores/"
+files = os.listdir(path_scores)
+files = [f for f in files if not f.endswith(".zip")]
+files = [f for f in files if not f.startswith(".")]
+df_scores = []
+for file in files: 
+	df_scores.append(pd.read_csv(os.path.join(path_scores, file)))
+
+df = pd.concat(df_scores)
+df["attention_scores"] = df["attention_scores"].apply(parse_attention_string)
+
+# Now, merge df_annot into df
+roi_colnames = ['setup_indices', 'exit_indices', 'manipulation_indices', 'return_indices', 'query_indices']
+
+df_merged = df.merge(df_annot[["tokenized_passage"] + roi_colnames], on="tokenized_passage", how="left")
+
+df_test = df_merged.iloc[:10]
+for roi in roi_colnames:
+	df_test[f"attn_avg_{roi}"] = df_test.apply(lambda row: mean_attn_roi(row["attention_scores"],row[roi]),axis=1)
